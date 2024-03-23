f2c6224e3bcc11f7ec91257d45c99ce40199b133
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2024-03-23 12:56:04 +0530

    new commit

---
+++ src/com/corejava/java8/funcinterface/FunctionalInterfaceEx.java
@@ -0,0 +1,105 @@
+package com.corejava.java8.funcinterface;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class FunctionalInterfaceEx {
+    // Consumer
+    @Test
+    public void printCities() {
+
+        List<String> cities = new ArrayList<>();
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Goa");
+        cities.add("Pune");
+
+        // Using a Consumer to convert names to uppercase
+        Consumer<String> convertToUppercase = name -> System.out.println("Converted name: " + name.toUpperCase());
+
+        // Applying the Consumer to each name
+        cities.forEach(convertToUppercase);
+
+
+        Consumer<String> printUpperCase = str -> System.out.println(str.toUpperCase());
+        printUpperCase.accept("hello"); //  HELLO
+
+      /*  Converted name: DELHI
+          Converted name: MUMBAI
+          Converted name: GOA
+          Converted name: PUNE
+       */
+    }
+
+    // Predicate
+    @Test
+    public void filterCities() {
+
+        List<String> cities = new ArrayList<>();
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Goa");
+        cities.add("Pune");
+
+        Predicate<String> filterCity = city -> city.equals("Mmbai");
+        cities.stream()
+                .filter(filterCity)
+                .forEach(System.out::println); // Mumbai
+
+        Predicate<Integer> isEven = num -> num % 2 == 0;
+        System.out.println(isEven.test(4)); // Output: true
+        System.out.println(isEven.test(7)); // Output: false
+    }
+
+    // Function
+    @Test
+    public void mapCities() {
+
+        List<String> cities = new ArrayList<>();
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Goa");
+        cities.add("Pune");
+
+        Function<String, Character> getFirstCharFunction = city -> city.charAt(0);
+        cities.stream()
+                .map(getFirstCharFunction)
+                .forEach(System.out::println);
+
+        /* D
+           M
+           G
+           P
+        */
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        Function<Integer, Integer> squareFunction = num -> num * num;
+        numbers.stream()
+                .map(squareFunction)
+                .forEach(result -> System.out.print(result + " "));
+
+        // 1 4 9 16 25
+    }
+
+    // Supplier
+    @Test
+    public void supplyCities() {
+
+        Supplier<Double> randomNumberSupplier = Math::random;
+        System.out.println(randomNumberSupplier.get()); // 0.017852732689994877
+
+        Supplier<String> greetingsSupplier = () -> {
+            String[] greetings = {"Hello", "Bonjour", "Hola", "Namaste"};
+            int randomIndex =  (((int) (Math.random() * 10)) % greetings.length );
+            return greetings[randomIndex];
+        };
+        System.out.println(greetingsSupplier.get()); // Namaste
+    }
+}
--- src/com/corejava/java8/funcinterface/func.md
+++ src/com/corejava/java8/funcinterface/func.md
@@ -30,3 +30,55 @@ public class Main {
         System.out.println("Result of addition: " + result);
     }
 }
+```
+
+# Consumer Interface in Java
+
+The `Consumer` interface in Java is an in-built functional interface in the `java.util.function` package. It is used when there's a need to consume objects, where the consumer takes an input value and returns nothing. The `Consumer` interface consists of two methods:
+
+## 1. `accept(T value)`
+
+This method is used to accept an input value of type `T` and perform some operation on it. It returns void, indicating that it does not produce any result.
+
+```java
+void accept(T value);
+```
+
+# Predicate Interface in Java
+
+The `Predicate` interface in Java is a functional interface that accepts an argument of type `T` and returns a boolean value. It is commonly used to apply filters to collections of objects.
+
+## 1. `boolean test(T value)`
+
+This method evaluates the predicate on the given input value and returns true if the input satisfies the predicate's condition, otherwise returns false.
+
+```java
+boolean test(T value);
+```
+
+# Function Interface in Java
+
+The `Function` interface in Java is another in-built functional interface in the `java.util.function` package. It represents a function that accepts one argument and produces a result. This interface is commonly used in the map feature of Stream APIs.
+
+## 1. `R apply(T var1)`
+
+This method applies this function to the given argument and returns the result.
+
+```java
+R apply(T var1);
+default <V> Function<V, R> compose(Function<V, T> before);
+default <V> Function<T, V> andThen(Function<R, V> after);
+static <T> Function<T, T> identity();
+````
+
+# Supplier Interface in Java
+
+The `Supplier` interface in Java is a part of the `java.util.function` package. It represents a function that does not take in any arguments but produces a value of type `T`. It contains only one method.
+
+## 1. `T get()`
+
+This method retrieves a value of type `T`.
+
+```java
+T get();
+```
