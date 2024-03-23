ee0c8838193dd80a1475cccc183f431ee7beffdd
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2024-03-23 11:29:41 +0530

    new commit

--- src/com/corejava/collections/List/LinkedListEx.java
+++ src/com/corejava/collections/List/LinkedListEx.java
@@ -15,19 +15,19 @@ public class LinkedListEx {
         friends.add("David");
         friends.add("Chris");
 
-        System.out.println("Initial LinkedList : " + friends);
+        System.out.println("Initial LinkedList : " + friends); // Initial LinkedList : [Rajeev, John, David, Chris]
 
         // Adding an element at the specified position in the LinkedList
         friends.add(3, "Lisa");
-        System.out.println("After add(3, \"Lisa\") : " + friends);
+        System.out.println("After add(3, \"Lisa\") : " + friends); // After add(3, "Lisa") : [Rajeev, John, David, Lisa, Chris]
 
         // Adding an element at the beginning of the LinkedList
         friends.addFirst("Steve");
-        System.out.println("After addFirst(\"Steve\") : " + friends);
+        System.out.println("After addFirst(\"Steve\") : " + friends); // After addFirst("Steve") : [Steve, Rajeev, John, David, Lisa, Chris]
 
         // Adding an element at the end of the LinkedList (This method is equivalent to the add() method)
         friends.addLast("Jennifer");
-        System.out.println("After addLast(\"Jennifer\") : " + friends);
+        System.out.println("After addLast(\"Jennifer\") : " + friends); // After addLast("Jennifer") : [Steve, Rajeev, John, David, Lisa, Chris, Jennifer]
 
         // Adding all the elements from an existing collection to the end of the LinkedList
         List<String> familyFriends = new ArrayList<>();
@@ -36,37 +36,25 @@ public class LinkedListEx {
 
         friends.addAll(familyFriends);
         System.out.println("After addAll(familyFriends) : " + friends);
+        // After addAll(familyFriends) : [Steve, Rajeev, John, David, Lisa, Chris, Jennifer, Jesse, Walt]
 
         // retrieving the first element of LinkedList
-        System.out.println("First element : " + friends.getFirst());
+        System.out.println("First element : " + friends.getFirst()); // First element : Steve
 
         // retrieving the last element of LinkedList
-        System.out.println("Last element : " + friends.getLast());
+        System.out.println("Last element : " + friends.getLast()); // Last element : Walt
 
         // removing the first element of LinkedList
         friends.removeFirst();
-        System.out.println("After removing First element : " + friends);
+        System.out.println("After removing First element : " + friends); // After removing First element : [Rajeev, John, David, Lisa, Chris, Jennifer, Jesse, Walt]
 
         // removing the last element of LinkedList
         friends.removeLast();
-        System.out.println("After removing Last element : " + friends);
+        System.out.println("After removing Last element : " + friends); // After removing Last element : [Rajeev, John, David, Lisa, Chris, Jennifer, Jesse]
 
         // Remove all the elements that satisfy the given predicate
         friends.removeIf(friend -> friend.startsWith("J"));
-        System.out.println("After removing elements starting with 'J': " + friends);
+        System.out.println("After removing elements starting with 'J': " + friends); // After removing elements starting with 'J': [Rajeev, David, Lisa, Chris]
 
     }
 }
-
-/*
-Initial LinkedList : [Rajeev, John, David, Chris]
-After add(3, "Lisa") : [Rajeev, John, David, Lisa, Chris]
-After addFirst("Steve") : [Steve, Rajeev, John, David, Lisa, Chris]
-After addLast("Jennifer") : [Steve, Rajeev, John, David, Lisa, Chris, Jennifer]
-After addAll(familyFriends) : [Steve, Rajeev, John, David, Lisa, Chris, Jennifer, Jesse, Walt]
-First element : Steve
-Last element : Walt
-After removing First element : [Rajeev, John, David, Lisa, Chris, Jennifer, Jesse, Walt]
-After removing Last element : [Rajeev, John, David, Lisa, Chris, Jennifer, Jesse]
-After removing elements starting with 'J': [Rajeev, David, Lisa, Chris]
-*/
