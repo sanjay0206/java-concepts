2d3b07e2c03d95887958fcb78c97cbb5aec5ca65
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2024-04-20 16:47:23 +0530

    new commit

--- src/com/corejava/basics/oops/runtimepolymorphism/Main.java
+++ src/com/corejava/basics/oops/runtimepolymorphism/Main.java
@@ -20,7 +20,8 @@ class Cat extends Animal {
     }
 }
 
-public class Main {
+public class Main{
+
     public static void main(String[] args) {
         Animal dog = new Dog();
         Animal cat = new Cat();
